1401bc2502a7871c9253bf28ca99421e680d6724
X0de <seanremington1221@gmail.com>
2026-05-21 23:06:53 +0300

    Add files via upload

---
+++ functions_examples.py
@@ -0,0 +1,32 @@
+# Basic function
+
+def say_hello():
+    print("Hello!")
+
+say_hello
+
+
+# Function with parameter
+
+def greet(name):
+    print("Hello", name)
+
+greet("Sean") 
+
+
+# Function with return value
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+
+print(result)
+
+
+# Anouther exampple
+
+def square(number):
+    return number * number
+
+print(square(4))
